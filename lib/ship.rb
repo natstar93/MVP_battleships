115a471ed57a77be05b36d244db31642af9a8d89
Siavosh Mahboubian <s.mahboubian1@gmail.com>
2015-06-16 11:41:29 +0100

    Added initialize to @position instance variable

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,6 +1,8 @@
 
 class Ship
 
+  attr_reader :position
+
   def initialize position
     @position = position
   end
--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -1,7 +1,11 @@
 require 'ship'
 
 describe Ship do
-
+  it 'has position when created' do
+    ship = Ship.new "A1"
+    expect(ship.position).to eq "A1"
+    # expect(subject).to respond_to(:initialize).with(1).argument
+  end
 
 
 

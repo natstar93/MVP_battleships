36be33f37acc404c90965bf0d1918a9612dd4cb0
Natalie Akam <natalie.akam@gmail.com>
2015-06-17 16:30:38 +0100

    Added hit, miss and sunk methods

--- lib/ship.rb
+++ lib/ship.rb
@@ -2,14 +2,37 @@
 class Ship
 
   attr_reader :position
+  attr_accessor :sunk
 
   def initialize position
     @position = position
     @hits = 0
+    @sunk = false
+    @size = 1
   end
 
   def hit position
   	@hits += 1
+  	if @hits == @size
+  	  sink
+  	end
+  	return @hits
+  end
+
+  def miss position
+    'Miss'
+  end
+
+  def second_hit position
+    'No more chance'
+  end
+
+  def sink
+    @sunk = true
+  end
+
+  def sunk?
+  	@sunk
   end
 
 end
--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -15,4 +15,27 @@ describe Ship do
   	ship = Ship.new "A1"
   	expect(ship.hit "A1").to eq 1
   end
+
+  it 'can be missed' do
+    ship = Ship.new "A1"
+    expect(ship.miss "B1").to eq 'Miss'
+  end
+
+  it 'will not be hit when it is missed' do
+    ship = Ship.new "A1"
+    expect(ship.miss "A1").to eq 'Miss'  
+    expect(ship.second_hit "A1").to eq 'No more chance' 
+  end
+
+  it 'is not sunk initially' do
+  	ship = Ship.new "A1"
+    expect(ship.sunk).to eq false
+  end
+
+  it 'can be sunk' do
+    ship = Ship.new "A1"
+    ship.sink
+    expect(ship).to be_sunk
+  end
+
 end
